9006c23fa1a7698a13cc3e4d443871c0db12c50a
Jerry Hilts <gwhilts@gmail.com>
2018-12-12 17:48:02 -0800

    Implement dummy view

--- src/Countdown.elm
+++ src/Countdown.elm
@@ -1,6 +1,7 @@
 module Countdown exposing (main)
 
-import Html
+import Html exposing (..)
+import Html.Attributes exposing (..)
 
 
 
@@ -8,10 +9,42 @@ import Html
 
 
 main =
-    Html.text "Hello, World!"
+    view
 
 
 
 -- Model
 -- View
+
+
+view =
+    div [ id "app" ]
+        [ header [ id "header", class "hero is-primary" ]
+            [ div [ id "title", class "hero-body" ]
+                [ h1 [ class "title has-text-centered" ]
+                    [ text "Countdown Cheater" ]
+                ]
+            ]
+        , section [ id "letters", class "columns" ]
+            [ div [ class "column has-text-centered" ]
+                [ button [ class "letterbox" ] [ text "A" ]
+                , button [ class "letterbox" ] [ text "T" ]
+                , button [ class "letterbox" ] [ text "C" ]
+                ]
+            ]
+        , section [ id "controls" ]
+            [ input [ placeholder "Type scrambled letters here" ]
+                []
+            ]
+        , hr [] []
+        , section [ id "found-words", class "section" ]
+            [ a [ class "button is-outlined  is-primary" ]
+                [ text "act" ]
+            , a [ class "button is-outlined  is-primary" ]
+                [ text "cat" ]
+            ]
+        ]
+
+
+
 -- Update
